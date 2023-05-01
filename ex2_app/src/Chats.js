8e1b7d1592d5ea372e450fb65c2374e2feb37eca
arielbarmats <ariel.barmats@gmail.com>
2023-05-01 22:04:36 +0300

    json msg

---
+++ ex2_app/src/Chats.js
@@ -0,0 +1,94 @@
+var the_list = [
+  {
+    from: "L",
+    to: "Light",
+    text: "Hello Kira, Im L",
+    time: "10:30",
+  },
+  {
+    from: "Light",
+    to: "L",
+    text: "Im not kira! I will prove it to you",
+    time: "11:30",
+  },
+  {
+    from: "Light",
+    to: "L",
+    text: "Lock me up in a jail, if the killing wont stops than Im not kira!",
+    time: "12:30",
+  },
+  {
+    from: "L",
+    to: "Light",
+    text: "Hmmmmm interesting... Okay I will accept that.",
+    time: "13:30",
+  },
+  {
+    from: "L",
+    to: "Light",
+    text: "Hmmmmm interesting... Okay I will accept that.",
+    time: "13:30",
+  },
+  {
+    from: "L",
+    to: "Light",
+    text: "Hmmmmm interesting... Okay I will accept that.",
+    time: "13:30",
+  },
+  {
+    from: "L",
+    to: "Light",
+    text: "Hmmmmm interesting... Okay I will accept that.",
+    time: "13:30",
+  },
+  {
+    from: "L",
+    to: "Light",
+    text: "Hmmmmm interesting... Okay I will accept that.",
+    time: "13:30",
+  },
+  {
+    from: "L",
+    to: "Light",
+    text: "Hmmmmm interesting... Okay I will accept that.",
+    time: "13:30",
+  },
+  {
+    from: "L",
+    to: "Light",
+    text: "Hmmmmm interesting... Okay I will accept that.",
+    time: "13:30",
+  },
+  {
+    from: "L",
+    to: "Light",
+    text: "Hmmmmm interesting... Okay I will accept that.",
+    time: "13:30",
+  },
+  {
+    from: "L",
+    to: "Light",
+    text: "Hmmmmm interesting... Okay I will accept that.",
+    time: "13:30",
+  },
+  {
+    from: "L",
+    to: "Light",
+    text: "Hmmmmm interesting... Okay I will accept that.",
+    time: "13:30",
+  },
+  {
+    from: "L",
+    to: "Light",
+    text: "Hmmmmm interesting... Okay I will accept that.",
+    time: "13:30",
+  },
+  {
+    from: "L",
+    to: "Light",
+    text: "Hmmmmm interesting... Okay I will accept that.",
+    time: "13:30",
+  }
+];
+
+export default the_list;
