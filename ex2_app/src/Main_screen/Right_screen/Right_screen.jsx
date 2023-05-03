165e98c0254288f629b66c597a0f7cec19d3e38e
Eviassor789 <102507977+Eviassor789@users.noreply.github.com>
2023-05-04 01:44:55 +0300

    send with enter

--- ex2_app/src/Main_screen/Right_screen/Right_screen.jsx
+++ ex2_app/src/Main_screen/Right_screen/Right_screen.jsx
@@ -1,14 +1,18 @@
 import Corrent_chat_name_area from "./Corrent_chat_name_area/Corrent_chat_name_area";
 import Chat_area from "./Chat_area/Chat_area";
 import Input_area from "./Input_area/Input_area";
+import { useState } from "react";
 
 function Right_screen() {
+
+  const [Details, SetDetails] = useState(0);
+
   return (
     <>
       <div id="right_screen" className="col col-8">
         <Corrent_chat_name_area />
         <Chat_area />
-        <Input_area />
+        <Input_area setState={SetDetails} state={Details}/>
       </div>
     </>
   );
